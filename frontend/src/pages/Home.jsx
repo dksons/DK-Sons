902def217ccab3e13bc79021e0b887c35878ecff
Ahmar Faraz <Ahmarfaraz9@gmail.com>
2025-04-29 03:27:22 +0530

    Recovered local changes after accidental pull

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import HeroSection from '@/components/HeroSection'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <HeroSection/>
+    {/*       
+            Rest of the components of Homepage
+
+        */}
+    </div>
+  )
+}
+
+export default Home
