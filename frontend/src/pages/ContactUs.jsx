2da1175f9625373e5a817bad640dd6b803fe3513
shahan <johndoe@email.com>
2025-05-12 20:20:51 +0530

    contac us form ui  bug done

--- frontend/src/pages/ContactUs.jsx
+++ frontend/src/pages/ContactUs.jsx
@@ -10,7 +10,7 @@ const ContactUs = () => {
 
  
 
-    <div className="pt-10 text-white bg-brown-vignette">
+    <div className="pt-10 text-white ">
       {/* Section 1 */}
 
       <section className="mx-auto mt-20 mb-10 flex w-11/12 max-w-maxContent flex-col justify-between gap-10 text-white lg:flex-row ">
