1eb4bb44c2bb8fbf2d22fff94d4f696ce2f83a86
Ahmar Faraz <Ahmarfaraz9@gmail.com>
2025-06-02 15:43:56 +0530

    new changes in prod

--- server/config/Db/DbConnect.js
+++ server/config/Db/DbConnect.js
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
 
 
-const DbConnect=async()=>{
-       await mongoose.connect(`${process.env.MONGODB_URL}/${process.env.DB_NAME}`).then(() => console.log('Connected!'))
-  .catch(err => console.error('Failed to connect', err));
-   
+const DbConnect = async()=>{
+  await mongoose.connect(process.env.MONGODB_URL,{
+  })
+  .then(() => console.log("DB CONNECTION SUCCESSFULL"))
+  .catch((error) => {
+      console.log("DB CONNECTION FAILED");
+      console.error(error);
+      process.exit(1); 
+  })
 }
 
 export default DbConnect
