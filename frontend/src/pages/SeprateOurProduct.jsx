6377b4363806967fe7e1fb724b178f69c6846e4f
dksons <samad@dytes.co>
2025-06-02 18:01:30 +0530

    Update SeprateOurProduct.jsx

--- frontend/src/pages/SeprateOurProduct.jsx
+++ frontend/src/pages/SeprateOurProduct.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function SeprateOurProduct() {
   return (
-    <div className='w-screen lg:h-screen flex justify-center items-center'>
+    <div className='w-screen lg:h-screen flex justify-center items-center lg:mt-0 mt-15'>
       <OurProducts/>
     </div>
   )
